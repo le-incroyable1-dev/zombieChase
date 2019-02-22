d091bd0cdef4044ac0ce1a5c193aa59113157964
le-incroyable1-dev <47893192+le-incroyable1-dev@users.noreply.github.com>
2019-02-22 23:28:21 +0530

    Add files via upload

---
+++ GoBack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBack : MonoBehaviour {
+
+    public float translatespeed;
+	// Use this for initialization
+	/*void Start () {
+		
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+
+        StartCoroutine(MoveBackward());
+
+
+		
+	}
+
+    private IEnumerator MoveBackward()
+    {
+
+
+        transform.Translate(translatespeed, 0, 0);
+
+        yield return null;
+
+    }
+}
---
+++ Tile_Spawner.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Tile_Spawner : MonoBehaviour {
+
+    
+
+    //public float translatespeed;
+    public GameObject[] tiles;
+    public Transform[] spawnPos;
+    public float SpawnRepeatRate;
+
+    // Use this for initialization
+    void Start () {
+
+        InvokeRepeating("SpawnTiles", 0.5f, SpawnRepeatRate);
+
+    }
+
+    // Update is called once per frame
+    /*void Update () {
+
+        StartCoroutine(MoveBackward());
+
+        
+
+    }
+
+    private IEnumerator MoveBackward()
+    {
+        
+
+        GameObject.FindGameObjectWithTag("Tile").transform.Translate(translatespeed, 0, 0);
+
+        yield return null;
+
+    }*/
+
+     
+    void SpawnTiles()
+    {
+        Quaternion rotation = new Quaternion(0, 0, 0, 0);
+        Instantiate(tiles[(int)(UnityEngine.Random.Range(0, tiles.Length))], spawnPos[(int)(UnityEngine.Random.Range(0, spawnPos.Length))].position, rotation);
+
+    }
+
+
+ 
+    
+   
+}
